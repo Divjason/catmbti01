1bc62e6bdb04cfd105c679317c8575ce0a3f8e48
Divjason <sejinpark@tcocs.com>
2024-10-03 19:16:06 +0900

    edit kakao message

--- src/components/KakaoShareButton.jsx
+++ src/components/KakaoShareButton.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { Button } from "react-bootstrap";
 const { Kakao } = window;
 
-const KakaoShareButton = () => {
+const KakaoShareButton = ({ data }) => {
   const url = "https://catmbti01.netlify.app/";
   const resultURL = window.location.href;
 
@@ -16,13 +16,11 @@ const KakaoShareButton = () => {
       objectType: "feed",
       content: {
         title: "예비집사 판별기 결과",
-        description:
-          "예비집사님이 고양이를 키운다면 가장 잘 맞는 고양이는 아비시니안 입니다",
-        imageUrl:
-          "https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg",
+        description: `예비집사님이 고양이를 키운다면 가장 잘 맞는 고양이는 ${data.name} 입니다`,
+        imageUrl: `${url}${data.image}`,
         link: {
-          mobileWebUrl: "https://developers.kakao.com",
-          webUrl: "https://developers.kakao.com",
+          mobileWebUrl: resultURL,
+          webUrl: resultURL,
         },
       },
       buttons: [
@@ -30,6 +28,7 @@ const KakaoShareButton = () => {
           title: "나도 테스트 하러가기",
           link: {
             mobileWebUrl: url,
+            webUrl: url,
           },
         },
       ],
